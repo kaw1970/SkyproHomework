77f09fc50a5038679c18badc37cff0f22db9fc03
Andrej Kudrjashow <kudrjashow@icloud.com>
2024-07-10 13:57:20 +0300

    исправление работы функций под номер карты с пробелами

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску.
     7000 79** **** 6361"""
-    return f"{int(str(card_number)[:4])} {int(str(card_number)[4:6])}** **** {int(str(card_number)[13:])}"
+    return f"{int(str(card_number)[:4])} {int(str(card_number)[4:6])}** **** {int(str(card_number)[12:])}"
 
 
 def get_mask_account(account_number: int) -> str:
--- src/widget.py
+++ src/widget.py
@@ -6,14 +6,18 @@ def mask_account_card(cart: str) -> str:
     Аргументом может быть строка типа Visa Platinum 7000792289606361
     , или Maestro 7000792289606361, или Счет 73654108430135874305.
     Разделять строку на 2 аргумента (отдельно имя, отдельно номер) нельзя!"""
-    list_cart_number = cart.split(" ")
-    if len(list_cart_number[-1]) == 16:
-        list_cart_number[-1] = masks.get_mask_card_number(int(list_cart_number[-1]))
-        mask_cart_numer = " ".join(list_cart_number)
-        return mask_cart_numer
+    name_cart = ''
+    numer_cart = ''
+    list_cart = cart.split()
+    for i in list_cart:
+        if i.isdigit():
+            numer_cart += i
+        elif i.isalpha():
+            name_cart += i + ' '
+    if len(numer_cart) == 16:
+        return name_cart + masks.get_mask_card_number(int(numer_cart))
     else:
-        list_cart_number[-1] = masks.get_mask_account(int(list_cart_number[-1]))
-        mask_score_numer = " ".join(list_cart_number)
+        mask_score_numer = name_cart + masks.get_mask_account(int(numer_cart))
     return mask_score_numer
 
 
@@ -23,4 +27,3 @@ def get_date(date: str) -> str:
     строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
     """
     return f'"{date[8:10]}.{date[5:7]}.{date[:4]}"'
-
