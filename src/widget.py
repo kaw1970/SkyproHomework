2ff3946d5685fe25ddf5e890540ad8c308c90fbc
Andrej Kudrjashow <kudrjashow@icloud.com>
2024-07-10 12:08:11 +0300

    добавление файлов которые должны отслеживаться, исправление функции get_date

--- src/widget.py
+++ src/widget.py
@@ -6,14 +6,14 @@ def mask_account_card(cart: str) -> str:
     Аргументом может быть строка типа Visa Platinum 7000792289606361
     , или Maestro 7000792289606361, или Счет 73654108430135874305.
     Разделять строку на 2 аргумента (отдельно имя, отдельно номер) нельзя!"""
-    list_cart_number = cart.split(' ')
+    list_cart_number = cart.split(" ")
     if len(list_cart_number[-1]) == 16:
         list_cart_number[-1] = masks.get_mask_card_number(int(list_cart_number[-1]))
-        mask_cart_numer = ' '.join(list_cart_number)
+        mask_cart_numer = " ".join(list_cart_number)
         return mask_cart_numer
     else:
         list_cart_number[-1] = masks.get_mask_account(int(list_cart_number[-1]))
-        mask_score_numer = ' '.join(list_cart_number)
+        mask_score_numer = " ".join(list_cart_number)
     return mask_score_numer
 
 
@@ -22,4 +22,5 @@ def get_date(date: str) -> str:
     функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"  и возвращает
     строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
     """
-    return f'"ДД.ММ.ГГГГ" ("{date[8:10]}.{date[5:7]}.{date[:4]}")'
+    return f'"{date[8:10]}.{date[5:7]}.{date[:4]}"'
+
