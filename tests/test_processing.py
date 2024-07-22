ab59294a6e7ea4b322336e017f298c4e3f8e60f3
Andrej Kudrjashow <kudrjashow@icloud.com>
2024-07-22 19:18:23 +0300

    исправления ошибок по flake8 в папке tests

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,32 +5,32 @@ from src.processing import filter_by_state, sort_by_date
 @pytest.fixture
 def filter_fixture():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 
 @pytest.fixture
 def filter_fixture_no_state():
     return [{'id': 41428829, 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}]
+            {'id': 939719570, 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}]
 
 
 @pytest.fixture
 def sort_the_same_dates():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}]
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}]
 
 
 @pytest.mark.parametrize('state, expected', [
-('EXECUTED', [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-('CANCELED', [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])])
+        ('EXECUTED', [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+        ('CANCELED', [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])])
 def test_filter_by_state(filter_fixture, state, expected):
     assert filter_by_state(filter_fixture, state) == expected
 
