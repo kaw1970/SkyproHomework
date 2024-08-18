5d101da38861492bdb731756adcf6f9ced6f0fda
Andrej Kudrjashow <kudrjashow@icloud.com>
2024-08-19 01:43:40 +0300

    связка функции transaction_amount c функцией currency_conversion модуля external_api

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import json
 from json import JSONDecodeError
 
+from src.external_api import currency_conversion
+
 
 def financial_transactions(path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
@@ -21,19 +23,25 @@ def transaction_amount(trans: dict, currency: str = "RUB") -> float:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
     if trans["operationAmount"]["currency"]["code"] == currency:
         amount = trans["operationAmount"]["amount"]
+    else:
+        amount = currency_conversion(trans)
     return amount
 
 
 if __name__ == '__main__':
     print(transaction_amount({
-    "id": 441945886,
+    "id": 41428829,
     "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
+    "date": "2019-07-03T18:35:29.512364",
     "operationAmount": {
-      "amount": "31957.58",
+      "amount": "8221.37",
       "currency": {
-        "name": "руб.",
-        "code": "RUB"
+        "name": "USD",
+        "code": "USD"
       }
-    }}
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  }
     ))
