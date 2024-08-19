f26dcd7c086a8f34508e0a9caf9f1eeed0309eef
Andrej Kudrjashow <kudrjashow@icloud.com>
2024-08-19 13:13:14 +0300

    исправление external_api.py, создание шаблона .env.example

--- src/external_api.py
+++ src/external_api.py
@@ -1,5 +1,12 @@
 import requests
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+values = os.getenv("PASSWORD")
+keys = os.getenv("API_KEY")
+headers = {keys:values}
 
 def currency_conversion(transaction: dict) -> float:
     """Функциz конвертации"""
@@ -8,7 +15,24 @@ def currency_conversion(transaction: dict) -> float:
     to = "RUB"
     url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amout}"
     payload = {}
-    headers = {"apikey": "3unFbSBaLFw7dwlSFoduXLcUmsNnI3xM"}
     response = requests.get(url, headers=headers, data=payload)
     result = response.json()
     return result["result"]
+
+
+if __name__ == '__main__':
+    print(currency_conversion({
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  }))
